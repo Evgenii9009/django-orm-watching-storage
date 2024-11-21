c3927ab1d754de7e65103fed8f8ec0e679753c51
Evgenii9009 <uptiik02@gmail.com>
2024-11-21 11:55:15 +0300

    Summary

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,39 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
+import os
+import django
+import datetime
+from models import Passcard
+from models import Visit
 from django.shortcuts import render
+
+from django.utils.timezone import make_aware
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+django.setup()
+
+
+def get_duration(visit):
+    now = make_aware(datetime.datetime.now())
+    then = visit.entered_at
+    delta = now - then
+    return delta
+
+def format_duration(duration):
+    minutes = (duration.total_seconds() % 3600) // 60
+    return minutes, "minutes"
 
 
 def storage_information_view(request):
-    # Программируем здесь
-
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    active_visits = Visit.objects.filter(leaved_at__isnull=True)
+    non_closed_visits = list()
+    for visit in active_visits:
+        duration = get_duration(visit)
+        formated_duration = format_duration(duration)
+        person = []
+        person = {
+            "who entered": str(visit.passcard),
+            "entered_at": str(visit.entered_at),
+            "duration": formated_duration
+            }
+        non_closed_visits.append(person)
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
